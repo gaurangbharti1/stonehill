02caeb78a1f9b5671a12a91ac8800d6425081da5
Rahul Tarak <rahultarak12345@gmail.com>
2019-03-22 07:06:11 +0000

    Change extensions

--- ML/Inception/ML/run.py
+++ ML/Inception/ML/run.py
@@ -1,3 +1,2 @@
 import os
-for i in range(1,10):
-	os.system("python3 /home/rahultarak12345/stonehill/ML/Inception/ML/retrain.py --bottleneck_dir=/home/rahultarak12345/stonehill/ML/Inception/ML/bottlenecks --how_many_training_steps 1000  --model_dir=/home/rahultarak12345/stonehill/ML/Inception/ML/ --output_graph=/home/rahultarak12345/stonehill/ML/Inception/ML/retrained_graph.pb --output_labels=/home/rahultarak12345/stonehill/ML/Inception/ML/retrained_labels.txt --image_dir=/home/rahultarak12345/stonehill/ML/data")
+os.system("python3 /home/rahultarak12345/stonehill/ML/Inception/ML/retrain.py --bottleneck_dir=/home/rahultarak12345/stonehill/ML/Inception/ML/bottlenecks --how_many_training_steps 1000  --model_dir=/home/rahultarak12345/stonehill/ML/Inception/ML/ --output_graph=/home/rahultarak12345/stonehill/ML/Inception/ML/retrained_graph.pb --output_labels=/home/rahultarak12345/stonehill/ML/Inception/ML/retrained_labels.txt --image_dir=/home/rahultarak12345/stonehill/ML/data")
