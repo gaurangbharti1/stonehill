ec3934e8c4822ad2716f7749b47fb15925cf571d
Rahul Tarak <rahultarak12345@gmail.com>
2019-03-22 10:43:14 +0000

    OS system

--- ML/Inception/ML/crop.py
+++ ML/Inception/ML/crop.py
@@ -1,6 +1,7 @@
-import sys
+import sys,os
 from PIL import Image
 im = Image.open(sys.argv[1])
 
 im.thumbnail((100, 100), Image.ANTIALIAS)
-im.save("smaller-output.png")
+im.save("smaller-output.jpg")
+os.system("python3 ./label_image.py smaller-output.jpg")
