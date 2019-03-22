a3b2e55a3cc10d610c74f8c78d73355bf880fd4f
Rahul Tarak <rahultarak12345@gmail.com>
2019-03-22 10:45:12 +0000

    OS system

--- ML/Inception/ML/crop.py
+++ ML/Inception/ML/crop.py
@@ -4,4 +4,4 @@ im = Image.open(sys.argv[1])
 
 im.thumbnail((100, 100), Image.ANTIALIAS)
 im.save("smaller-output.jpg")
-os.system("python3 ./label_image.py smaller-output.jpg")
+os.system("python3 ML/Inception/ML/label_image.py smaller-output.jpg")
