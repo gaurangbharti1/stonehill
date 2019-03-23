98f984041799d1123f0df9b4a096f359a49fd309
Rahul Tarak <rahultarak12345@gmail.com>
2019-03-23 03:23:27 +0000

    int growth

--- image.py
+++ image.py
@@ -81,8 +81,8 @@ data = r.json()
 #print(data)
 print("Other Relevant Statistics:")
 #print(data['data'][1])
-temp1 =  data['data'][0]['coordinates'][0]['dates'][0]['value']
-temp2 =  data['data'][1]['coordinates'][0]['dates'][0]['value']
+temp1 = data['data'][0]['coordinates'][0]['dates'][0]['value']
+temp2 = data['data'][1]['coordinates'][0]['dates'][0]['value']
 temp3 = data['data'][2]['coordinates'][0]['dates'][0]['value']
 print("Temperature (C): " + str(temp1))
 print("Effective Humidity" + str(temp2)])
